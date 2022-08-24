f27d8ed3246aaffaa658f065623e2f74391b9b5d
agaxe <agaxe.dev@gmail.com>
2022-08-24 19:47:37 +0900

    first commit

---
+++ yarn/index.js
@@ -0,0 +1,15 @@
+const moment = require('moment');
+const shortId = require('shortid');
+const { faker } = require('@faker-js/faker');
+const firebaseAdmin = require('firebase-admin');
+
+const result = {
+  id: shortId.generate(),
+  name: faker.internet.userName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  birthdate: faker.date.birthdate(),
+  createdAt: moment().format()
+};
+
+console.log('result (yarn)', result);
